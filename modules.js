7b4d563687511d245487b52b33e58159b7d4d225
Muhammad Iftikhar Hafeez <attsopal@gmail.com>
2021-05-20 20:05:33 -0400

    added credentials for mongodb

--- modules.js
+++ modules.js
@@ -4,29 +4,11 @@ const FlatFile = require('./flatfile_database');
 const mongoose = require('mongoose');
 module.exports = () => {
 
-    mongoose.connect('mongodb://localhost:27017/meetings', {useNewUrlParser: true});
+    mongoose.connect('mongodb://localhost:27017/meetings', {
+        user: 'iftikhar',
+        pass: process.env.mongo_pass,
+        useNewUrlParser: true
+    });
 
-    const meetings = '/Users/attsopal/Documents/Hypercare/engineering/repos/meetings.txt'
-    const users = '/Users/attsopal/Documents/Hypercare/engineering/repos/users.txt'
-    const gData = '/Users/attsopal/Documents/Hypercare/engineering/repos/gData.txt'
-    if(!fs.existsSync(meetings)){
-        fs.writeFileSync(meetings, JSON.stringify([]));
-    }
-    const meetingsDb = new FlatFile(meetings); 
-
-    if(!fs.existsSync(users)){
-        fs.writeFileSync(users, JSON.stringify({}));
-    }
-    const usersDb = new FlatFile(users); 
-
-    if(!fs.existsSync(gData)){
-        fs.writeFileSync(gData, JSON.stringify({}));
-    }
-    const gDataDb = new FlatFile(gData); 
-
-    return {
-        meetingsDb,
-        usersDb,
-        gDataDb
-    }
+    return {};
 }
